94b0cb7b97c87efc6f194ba39a9d0dfc422f4e50
DanielAmah <amahdanieljack@gmail.com>
2017-06-18 16:58:24 +0100

    index file

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './root.jsx';
+
+import './styles/style.scss';
+
+const main = document.getElementById('main');
+
+ReactDOM.render(<Root />, main);
