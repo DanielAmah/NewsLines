a2befd8ea99ea37528e10d44742a507043463269
DanielAmah <amahdanieljack@gmail.com>
2017-06-18 17:10:18 +0100

    flux stores

---
+++ src/store/NewsStore.js
@@ -0,0 +1,45 @@
+import ObjectAssign from 'object-assign';
+import dispatcher from '../dispatcher';
+import Constants from '../constants/constants';
+
+const EventEmitter = require('events').EventEmitter;
+
+
+const CHANGE_EVENT = 'change';
+
+const store = {
+  list: null,
+};
+
+
+const NewsStore = ObjectAssign({}, EventEmitter.prototype, {
+
+  addChangeListener(cb) {
+    this.on(CHANGE_EVENT, cb);
+  },
+  removeChangeListener(cb) {
+    this.removeListener(CHANGE_EVENT, cb);
+  },
+  getList() {
+    return store;
+  },
+  listInfo() {
+    return store.list;
+  },
+});
+
+dispatcher.register((payload) => {
+  const action = payload.action;
+  const newArticle = action.response;
+  switch (action.actionType) {
+    case Constants.NEW_NEWS:
+      store.list = newArticle;
+      NewsStore.emit(CHANGE_EVENT);
+      break;
+
+    default:
+      return true;
+  }
+});
+
+export default NewsStore;
