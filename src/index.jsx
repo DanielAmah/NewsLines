b6cae51681f77cb4936bb46b3cf17678a24e0f6b
DanielAmah <amahdanieljack@gmail.com>
2017-07-13 21:22:09 +0100

    refactor router authentication

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,3 @@
-/* global document localStorage*/
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
@@ -9,17 +8,42 @@ import Layout from './components/Layout.jsx';
 import Login from './components/Login.jsx';
 import './styles/style.scss';
 
-// Gets element with id:app
-const app = document.getElementById('main');
 
-// Renders in the react DOM
+const app = document.getElementById('main');
+/**
+ * @description Google authorisation url to verify user email.
+ */
+const verificationURL = 'https://www.googleapis.com/oauth2/v3/tokeninfo?' +
+  `id_token=${window.localStorage.userToken}`;
+
+/**
+ * @function {isUserLoggedIn} function to check verification.
+ * @description Verify google user token stored in local storage and authorise user
+ * redirect to protected routes after verification
+ * @return {Promise} a promise
+ * @
+ */
+const isUserLoggedIn = () => {
+  if (localStorage.userToken) {
+    return fetch(verificationURL)
+      .then(res => res)
+      .then((googleResponse) => {
+        if (googleResponse.email_verified === 'true') {
+          return true;
+        }
+        return false;
+      });
+  }
+  return false;
+};
+
 ReactDom.render(
   <Router history={browserHistory}>
     <Route component={Layout}>
-      <Route path="/" component={localStorage.User ? SourceSort : Login} />
+      <Route path="/" component={isUserLoggedIn() ? SourceSort : Login} />
       <Route
         path="news"
-        component={localStorage.User ? NewsList : Login}
+        component={isUserLoggedIn() ? NewsList : Login}
       />
     </Route>
   </Router>,
